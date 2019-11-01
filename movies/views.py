5cede6745e950f49c2baa3761140165029e21382
subinchoe <ch960318@naver.com>
2019-11-01 10:45:22 +0900

    views.py GET_genres

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Genre
+from .serializers import GenreSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def genres(request):
+    genre_all = Genre.objects.all()
+    serializer = GenreSerializer(genre_all, many=True)
+    return Response(serializer.data)
