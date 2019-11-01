4bf9afc9013986ccfacc924ca08736c6f9a25c6f
subinchoe <ch960318@naver.com>
2019-11-01 15:16:50 +0900

    schema

--- api/urls.py
+++ api/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title = 'Movie API',
+        default_version = 'v1',
+    ),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('movies.urls'))
+    path('api/v1/', include('movies.urls')),
+    path('redoc/', schema_view.with_ui('redoc')),
+    path('swagger/', schema_view.with_ui('swagger')),
 ]
