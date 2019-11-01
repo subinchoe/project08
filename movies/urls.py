102dcaaabad009f9d0f6b9ffd8e798da93538679
subinchoe <ch960318@naver.com>
2019-11-01 11:41:42 +0900

    urls.py review_create

--- movies/urls.py
+++ movies/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('genres/<int:genre_pk>/', views.genre_detail),
     path('movies/', views.movies),
     path('movies/<int:movie_pk>/', views.movie_detail),
+    path('movies/<int:movie_pk>/reviews/', views.review_create),
 ]
