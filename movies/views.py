9c856834246cfc0c8f5a00b526776e820632c67a
subinchoe <ch960318@naver.com>
2019-11-01 10:59:23 +0900

    views.py GET_genre_detail

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Genre
 from .serializers import GenreSerializer
 from rest_framework.decorators import api_view
@@ -8,4 +8,10 @@ from rest_framework.response import Response
 def genres(request):
     genre_all = Genre.objects.all()
     serializer = GenreSerializer(genre_all, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def genre_detail(request, genre_pk):
+    genre = get_object_or_404(Genre, id=genre_pk)
+    serializer = GenreSerializer(genre)
     return Response(serializer.data)
